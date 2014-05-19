06b8f4919dbfb90a57299faa3622389d01e8b83f
wzaghal <wisamzaghal@gmail.com>
2014-05-19 16:00:36 -0400

    add basic js until twitter api functionality is added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //temporary until we replace with API calls
 var user = { 
-	'name':'@proudfoot',
+	'name':'@proudfeet',
 	'tweet': "I made where's drake",
 	'imageUrl':'https://pbs.twimg.com/profile_images/458796625469534209/rHBlhlD6.jpeg'
 }
@@ -9,3 +9,5 @@ var wrongUser= {
 	'name': '@_wzaghal',
 	'imageUrl': 'https://pbs.twimg.com/profile_images/459722214279884800/7LuzEpXC_400x400.jpeg'
 }
+
+
